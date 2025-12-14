0435200690864c207c79a06c29834e706e519dd7
Jack Armstrong <jackarmstrong819@gmail.com>
2025-12-14 23:30:40 +0000

    f

---
+++ task_03/question_10.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let leftLink = document.getElementById("left");
+  let centerLink = document.getElementById("center");
+  let rightLink = document.getElementById("right");
+  let justifyLink = document.getElementById("justify");
+
