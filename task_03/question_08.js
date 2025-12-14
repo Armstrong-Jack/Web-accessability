442e3e98f6f78b272dd31edb04d6a0a39fde626e
Jack Armstrong <jackarmstrong819@gmail.com>
2025-12-14 23:30:00 +0000

    f

---
+++ task_03/question_08.js
@@ -0,0 +1,18 @@
+/*jshint esnext: true, browser: true, devel:true */
+"use strict";
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  let link = document.getElementById("action");
+
+  
+  link.addEventListener("click", function(event) {
+    alert("why did you do that?");
+  });
+});
+
+
+
+
+
